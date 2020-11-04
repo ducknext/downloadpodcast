26d2eac629d4a05757359b3a8e7ad013fa37da1e
ducknext <inese.ieraga@gmail.com>
2020-11-04 12:38:39 +0100

    some fixes

--- podcast_app.py
+++ podcast_app.py
@@ -24,6 +24,11 @@ def csv_to_list(csv_file):
     return csv_data_first
 
 
+def my_hook(d):
+    if d['status'] == 'finished':
+        print('Done downloading, now converting ...')
+
+
 def download(video_id, saved_videos):
     ydl_opts = {
         'audio-format': 'bestaudio/best',
@@ -31,20 +36,23 @@ def download(video_id, saved_videos):
         # 'audioformat': 'mp3',  # convert to mp3
         'outtmpl': '%(id)s-%(uploader)s-%(title)s.%(ext)s',  # name the file
         'noplaylist': True,  # only single video, not playlist
+        'progress_hooks': [my_hook],
     }
 
     # TODO errors!
     if video_id in saved_videos:
-        return "Already downloaded"
+        return None
 
     if len(video_id) != 11:
         return "Faulty ID"
-
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download(['https://www.youtube.com/watch?v={}'.format(video_id)])
+    try:
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download(['https://www.youtube.com/watch?v={}'.format(video_id)])
+    except:  # noqa
+        return "Youtube error"
 
     save_row = [video_id]
-    with open('yt_downloads.csv', 'a') as write_obj:
+    with open('downloads.csv', 'a') as write_obj:
         csv_writer = csv.writer(write_obj)
         csv_writer.writerow(save_row)
 
@@ -77,16 +85,17 @@ def get_youtube_link_ids(channel_id):
 
 
 def main():
-    channel_ids = csv_to_list('yt_channels.csv')
-    saved_videos = csv_to_list('yt_downloads.csv')
+    channel_ids = csv_to_list('channels.csv')
+    saved_videos = csv_to_list('downloads.csv')  # TODO make that it opens or creates a file
 
     for channel in channel_ids:
+        print(channel)
         video_ids = get_youtube_link_ids(channel)
 
         for video in video_ids:
             err = download(video, saved_videos)
             if err:
-                logging.info("Video ID {} for channel ID {} failed".format(video, channel))
+                logging.warning("Video ID {} for channel ID {} failed".format(video, channel))
 
 
 if __name__ == "__main__":
