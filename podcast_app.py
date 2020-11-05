9562f1eb0b70c3db235000c09b80e878b0de4633
ducknext <inese.ieraga@gmail.com>
2020-11-05 15:04:55 +0100

    changed from csv to json

--- podcast_app.py
+++ podcast_app.py
@@ -4,69 +4,7 @@ from __future__ import unicode_literals
 import youtube_dl
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
-import logging
-import csv
-
-logging.basicConfig(filename="podcastAppLog.log")
-
-
-def csv_to_list(csv_file):
-    full_csv_data = []
-    with open(csv_file) as csv_file:
-        csvReader = csv.reader(csv_file, delimiter=',')
-        for row in csvReader:
-            full_csv_data.append(row)
-
-    csv_data_first = []
-    for line in full_csv_data:
-        csv_data_first.append(line[0])
-
-    return csv_data_first
-
-
-def my_hook(d):
-    if d['status'] == 'finished':
-        print('Done downloading, now converting ...')
-
-
-def download(video_id, saved_videos):
-    ydl_opts = {
-        'audio-format': 'bestaudio/best',
-        # 'extractaudio': True,  # only keep the video
-        # 'audioformat': 'mp3',  # convert to mp3
-        'outtmpl': '%(id)s-%(uploader)s-%(title)s.%(ext)s',  # name the file
-        'noplaylist': True,  # only single video, not playlist
-        'progress_hooks': [my_hook],
-    }
-
-    # TODO errors!
-    if video_id in saved_videos:
-        return None
-
-    if len(video_id) != 11:
-        return "Faulty ID"
-    try:
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl.download(['https://www.youtube.com/watch?v={}'.format(video_id)])
-    except:  # noqa
-        return "Youtube error"
-
-    save_row = [video_id]
-    with open('downloads.csv', 'a') as write_obj:
-        csv_writer = csv.writer(write_obj)
-        csv_writer.writerow(save_row)
-
-    return None
-
-
-def get_meta():
-
-    ydl_opts = {}
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        meta = ydl.extract_info(
-            'https://www.youtube.com/watch?v=dP15zlyra3c', download=False)
-
-    print(meta)
+import json
 
 
 def get_youtube_link_ids(channel_id):
@@ -77,25 +15,59 @@ def get_youtube_link_ids(channel_id):
     video_ids = []
     for x in full_yt_video_id:
         x = str(x)
-        x = x.lstrip('<yt:videoid>')
-        x = x.rstrip('</yt:videoid>')
-        video_ids.append(x)
+        x = x.split('<yt:videoid>')
+        x = x[1].split('</yt:videoid>')
+        video_ids.append(x[0])
 
     return video_ids
 
 
-def main():
-    channel_ids = csv_to_list('channels.csv')
-    saved_videos = csv_to_list('downloads.csv')  # TODO make that it opens or creates a file
+def my_hook(d):
+    if d['status'] == 'finished':
+        print('Done downloading, now converting ...')
 
-    for channel in channel_ids:
-        print(channel)
-        video_ids = get_youtube_link_ids(channel)
+
+def download(video_id):
+    ydl_opts = {
+        'audio-format': 'bestaudio/best',
+        # 'extractaudio': True,  # only keep the video
+        # 'audioformat': 'mp3',  # convert to mp3
+        'outtmpl': '%(id)s-%(uploader)s-%(title)s.%(ext)s',  # name the file
+        'noplaylist': True,  # only single video, not playlist
+        'progress_hooks': [my_hook],
+    }
+
+    try:
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download(['https://www.youtube.com/watch?v={}'.format(video_id)])
+    except youtube_dl.utils.DownloadError:
+        print("Youtube Error")
+
+
+def main():
+    with open('.podcast_channels.json') as f:
+        channels = json.load(f)
+
+    for channel in channels:
+        video_ids = get_youtube_link_ids(channel['id'])
 
         for video in video_ids:
-            err = download(video, saved_videos)
-            if err:
-                logging.warning("Video ID {} for channel ID {} failed".format(video, channel))
+            with open('.podcast_downloads.json') as f:
+                downloads = json.load(f)
+
+            download_ids = []
+            for item in downloads:
+                download_ids.append(item['id'])
+
+            if video not in download_ids:
+
+                err = download(video)
+                if err:
+                    return err
+                downloads.append({"id": video})
+
+            with open('.podcast_downloads.json', 'w') as f:
+                json.dump(downloads, f)
 
 
 if __name__ == "__main__":
