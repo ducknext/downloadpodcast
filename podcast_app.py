4f3ddce5c3fd72416bf92005de6cd374bbb97269
ducknext <inese.ieraga@gmail.com>
2020-11-09 18:16:11 +0100

    refactoring the code

--- podcast_app.py
+++ podcast_app.py
@@ -3,23 +3,15 @@
 from __future__ import unicode_literals
 import youtube_dl
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
+import requests
 import json
 
 
 def get_youtube_link_ids(channel_id):
-    html = urlopen('https://www.youtube.com/feeds/videos.xml?channel_id={}'.format(channel_id))
-    soupeddata = BeautifulSoup(html, 'html.parser')
-    full_yt_video_id = soupeddata.find_all('yt:videoid')
+    page = requests.get('https://www.youtube.com/feeds/videos.xml?channel_id={}'.format(channel_id))
+    soupeddata = BeautifulSoup(page.content, 'html.parser')
 
-    video_ids = []
-    for x in full_yt_video_id:
-        x = str(x)
-        x = x.split('<yt:videoid>')
-        x = x[1].split('</yt:videoid>')
-        video_ids.append(x[0])
-
-    return video_ids
+    return [x.string for x in soupeddata.find_all('yt:videoid')]
 
 
 def my_hook(d):
@@ -27,7 +19,7 @@ def my_hook(d):
         print('Done downloading, now converting ...')
 
 
-def download(video_id):
+def download(video, downloads):
     ydl_opts = {
         'audio-format': 'bestaudio/best',
         'outtmpl': '%(id)s-%(uploader)s-%(title)s.%(ext)s',  # name the file
@@ -38,9 +30,12 @@ def download(video_id):
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             ydl.download(
-                ['https://www.youtube.com/watch?v={}'.format(video_id)])
+                ['https://www.youtube.com/watch?v={}'.format(video)])
+
+        downloads.append({"id": video})
+        dump_file(downloads, '.podcast_downloadsss.json')
     except youtube_dl.utils.DownloadError:
-        print("Youtube Error")
+        print("Youtube Error with link https://www.youtube.com/watch?v={}".format(video))
 
 
 def dump_file(data_dump, file_name):
@@ -57,23 +52,27 @@ def load_file_or_fail(file_name):
         exit(1)
 
 
+def load_file_or_default(file_name, place_holder):
+    try:
+        with open(file_name) as f:
+            return json.load(f)
+    except:  # noqa
+        dump_file(place_holder, file_name)
+        with open(file_name) as f:
+            return json.load(f)
+
+
 def main():
     for channel in load_file_or_fail('.podcast_channels.json'):
         video_ids = get_youtube_link_ids(channel['id'])
 
         for video in video_ids:
             downloads = load_file_or_default(
-                '.podcast_downloads.json',
+                '.podcast_downloadsss.json',
                 [{"id": 'place_holder'}],
             )
-
-            if video not in downloads.keys():
-                err = download(video)
-                if err:
-                    print(err)
-                else:
-                    downloads.append({"id": video})
-                    dump_file(downloads, '.podcast_downloads.json')
+            if not any(download['id'] == video for download in downloads):
+                download(video, downloads)
 
 
 if __name__ == "__main__":
